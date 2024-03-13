af0d4e3653beb683a7daf431bd98feeafe5ab872
meysiolio <saturninsky@gmail.com>
2024-03-13 00:04:05 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+import email_validator
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+
+class contactForm(FlaskForm):
+    name = StringField(label='Name', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[
+        DataRequired(), Email(granular_message=True)])
+    message = StringField(label='Message')
+    submit = SubmitField(label="Log In")
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    cform = contactForm()
+    if cform.validate_on_submit():
+        print(f"Name: {cform.name.data}, E-mail: {cform.email.data}, message: {cform.message.data}")
+    else:
+        print("Invalid Credentials.")
+    return render_template("contact.html", form=cform)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
