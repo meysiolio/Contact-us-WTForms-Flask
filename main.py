1f84290438aa311ebc2832ea21315b9a8eb9185e
meysiolio <saturninsky@gmail.com>
2024-03-14 08:38:10 -0400

    Added flask bootstrap

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from wtforms import StringField, validators, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 import email_validator
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 class contactForm(FlaskForm):
     name = StringField(label='Name', validators=[DataRequired()])
-    email = StringField(label='Email', validators=[
-        DataRequired(), Email(granular_message=True)])
+    email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)])
     message = StringField(label='Message')
     submit = SubmitField(label="Log In")
 
